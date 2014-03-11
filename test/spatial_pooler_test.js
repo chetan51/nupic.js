19416db6348602c97348238e282e81e08f5f64c9
Chetan Surpur <chetan51@gmail.com>
2014-03-11 12:16:44 -0700

    Formatting

--- test/spatial_pooler_test.js
+++ test/spatial_pooler_test.js
@@ -1,153 +1,153 @@
 describe('SpatialPooler', function() {
 
-  describe('init', function() {
+    describe('init', function() {
 
-    it('should set up default params', function() {
-        var sp = new SpatialPooler(),
-            defaults = {
-                inputDimensions: [32,32],
-                columnDimensions: [64,64],
-                potentialRadius: 16,
-                potentialPct: 0.5,
-                globalInhibition: false,
-                localAreaDensity: -1.0,
-                numActiveColumnsPerInhArea: 10.0,
-                stimulusThreshold: 0,
-                synPermInactiveDec: 0.01,
-                synPermActiveInc: 0.1,
-                synPermConnected: 0.10,
-                minPctOverlapDutyCycle: 0.001,
-                minPctActiveDutyCycle: 0.001,
-                dutyCyclePeriod: 1000,
-                maxBoost: 10.0,
-                seed: -1,
-                spVerbosity: 0
-            },
-            numColumns = prod(defaults.columnDimensions),
-            numInputs = prod(defaults.inputDimensions),
-            iterationNum = 0,
-            iterationLearnNum = 0,
-            updatePeriod = 50,
-            synPermTrimThreshold = defaults.synPermActiveInc / 2.0,
-            synPermBelowStimulusInc = defaults.synPermConnected / 10.0;
-        
-        sp.getNumColumns().should.equal(numColumns);
-        sp.getNumInputs().should.equal(numInputs);
-        sp.getPotentialRadius().should.equal(defaults.potentialRadius);
-        sp.getPotentialPct().should.equal(defaults.potentialPct);
-        sp.getGlobalInhibition().should.equal(defaults.globalInhibition);
-        sp.getNumActiveColumnsPerInhArea().should.equal(defaults.numActiveColumnsPerInhArea);
-        sp.getLocalAreaDensity().should.equal(defaults.localAreaDensity);
-        sp.getStimulusThreshold().should.equal(defaults.stimulusThreshold);
-        // sp.getInhibitionRadius().should.equal();
-        sp.getDutyCyclePeriod().should.equal(defaults.dutyCyclePeriod);
-        sp.getMaxBoost().should.equal(defaults.maxBoost);
-        sp.getIterationNum().should.equal(iterationNum);
-        sp.getIterationLearnNum().should.equal(iterationLearnNum);
-        sp.getSpVerbosity().should.equal(defaults.spVerbosity);
-        sp.getUpdatePeriod().should.equal(updatePeriod);
-        sp.getSynPermTrimThreshold().should.equal(synPermTrimThreshold);
-        sp.getSynPermActiveInc().should.equal(defaults.synPermActiveInc);
-        sp.getSynPermInactiveDec().should.equal(defaults.synPermInactiveDec);
-        sp.getSynPermBelowStimulusInc().should.equal(synPermBelowStimulusInc);
-        sp.getSynPermConnected().should.equal(defaults.synPermConnected);
-        sp.getMinPctOverlapDutyCycles().should.equal(defaults.minPctOverlapDutyCycle);
-        sp.getMinPctActiveDutyCycles().should.equal(defaults.minPctActiveDutyCycle);
+        it('should set up default params', function() {
+            var sp = new SpatialPooler(),
+                defaults = {
+                    inputDimensions: [32,32],
+                    columnDimensions: [64,64],
+                    potentialRadius: 16,
+                    potentialPct: 0.5,
+                    globalInhibition: false,
+                    localAreaDensity: -1.0,
+                    numActiveColumnsPerInhArea: 10.0,
+                    stimulusThreshold: 0,
+                    synPermInactiveDec: 0.01,
+                    synPermActiveInc: 0.1,
+                    synPermConnected: 0.10,
+                    minPctOverlapDutyCycle: 0.001,
+                    minPctActiveDutyCycle: 0.001,
+                    dutyCyclePeriod: 1000,
+                    maxBoost: 10.0,
+                    seed: -1,
+                    spVerbosity: 0
+                },
+                numColumns = prod(defaults.columnDimensions),
+                numInputs = prod(defaults.inputDimensions),
+                iterationNum = 0,
+                iterationLearnNum = 0,
+                updatePeriod = 50,
+                synPermTrimThreshold = defaults.synPermActiveInc / 2.0,
+                synPermBelowStimulusInc = defaults.synPermConnected / 10.0;
+            
+            sp.getNumColumns().should.equal(numColumns);
+            sp.getNumInputs().should.equal(numInputs);
+            sp.getPotentialRadius().should.equal(defaults.potentialRadius);
+            sp.getPotentialPct().should.equal(defaults.potentialPct);
+            sp.getGlobalInhibition().should.equal(defaults.globalInhibition);
+            sp.getNumActiveColumnsPerInhArea().should.equal(defaults.numActiveColumnsPerInhArea);
+            sp.getLocalAreaDensity().should.equal(defaults.localAreaDensity);
+            sp.getStimulusThreshold().should.equal(defaults.stimulusThreshold);
+            // sp.getInhibitionRadius().should.equal();
+            sp.getDutyCyclePeriod().should.equal(defaults.dutyCyclePeriod);
+            sp.getMaxBoost().should.equal(defaults.maxBoost);
+            sp.getIterationNum().should.equal(iterationNum);
+            sp.getIterationLearnNum().should.equal(iterationLearnNum);
+            sp.getSpVerbosity().should.equal(defaults.spVerbosity);
+            sp.getUpdatePeriod().should.equal(updatePeriod);
+            sp.getSynPermTrimThreshold().should.equal(synPermTrimThreshold);
+            sp.getSynPermActiveInc().should.equal(defaults.synPermActiveInc);
+            sp.getSynPermInactiveDec().should.equal(defaults.synPermInactiveDec);
+            sp.getSynPermBelowStimulusInc().should.equal(synPermBelowStimulusInc);
+            sp.getSynPermConnected().should.equal(defaults.synPermConnected);
+            sp.getMinPctOverlapDutyCycles().should.equal(defaults.minPctOverlapDutyCycle);
+            sp.getMinPctActiveDutyCycles().should.equal(defaults.minPctActiveDutyCycle);
+        });
+
+        it('should allow overriding default params', function() {
+            var sp = new SpatialPooler({ potentialPct: 0.3 });
+
+            sp.getPotentialPct().should.equal(0.3);
+        });
+
+        it('should allow setting and setting params', function() {
+            var sp = new SpatialPooler(),
+                params = {
+                    potentialRadius: 8,
+                    potentialPct: 0.25,
+                    globalInhibition: true,
+                    localAreaDensity: 0.2,
+                    numActiveColumnsPerInhArea: 15.0,
+                    stimulusThreshold: 0.5,
+                    inhibitionRadius: 0.1,
+                    synPermInactiveDec: 0.2,
+                    synPermActiveInc: 0.2,
+                    synPermConnected: 0.20,
+                    minPctOverlapDutyCycle: 0.002,
+                    minPctActiveDutyCycle: 0.002,
+                    dutyCyclePeriod: 2000,
+                    maxBoost: 20.0,
+                    seed: -2,
+                    spVerbosity: 1,
+                    iterationNum: 1,
+                    iterationLearnNum: 2,
+                    updatePeriod: 10,
+                    synPermTrimThreshold: 0.2,
+                    synPermBelowStimulusInc: 0.3
+                };
+            
+            sp.setPotentialRadius(params.potentialRadius);
+            sp.getPotentialRadius().should.equal(params.potentialRadius);
+
+            sp.setPotentialPct(params.potentialPct);
+            sp.getPotentialPct().should.equal(params.potentialPct);
+
+            sp.setGlobalInhibition(params.globalInhibition);
+            sp.getGlobalInhibition().should.equal(params.globalInhibition);
+
+            sp.setNumActiveColumnsPerInhArea(params.numActiveColumnsPerInhArea);
+            sp.getNumActiveColumnsPerInhArea().should.equal(params.numActiveColumnsPerInhArea);
+
+            sp.setLocalAreaDensity(params.localAreaDensity);
+            sp.getLocalAreaDensity().should.equal(params.localAreaDensity);
+
+            sp.setStimulusThreshold(params.stimulusThreshold);
+            sp.getStimulusThreshold().should.equal(params.stimulusThreshold);
+
+            sp.setInhibitionRadius(params.inhibitionRadius);
+            sp.getInhibitionRadius().should.equal(params.inhibitionRadius);
+
+            sp.setDutyCyclePeriod(params.dutyCyclePeriod);
+            sp.getDutyCyclePeriod().should.equal(params.dutyCyclePeriod);
+
+            sp.setMaxBoost(params.maxBoost);
+            sp.getMaxBoost().should.equal(params.maxBoost);
+
+            sp.setIterationNum(params.iterationNum);
+            sp.getIterationNum().should.equal(params.iterationNum);
+
+            sp.setIterationLearnNum(params.iterationLearnNum);
+            sp.getIterationLearnNum().should.equal(params.iterationLearnNum);
+
+            sp.setSpVerbosity(params.spVerbosity);
+            sp.getSpVerbosity().should.equal(params.spVerbosity);
+
+            sp.setUpdatePeriod(params.updatePeriod);
+            sp.getUpdatePeriod().should.equal(params.updatePeriod);
+
+            sp.setSynPermTrimThreshold(params.synPermTrimThreshold);
+            sp.getSynPermTrimThreshold().should.equal(params.synPermTrimThreshold);
+
+            sp.setSynPermActiveInc(params.synPermActiveInc);
+            sp.getSynPermActiveInc().should.equal(params.synPermActiveInc);
+
+            sp.setSynPermInactiveDec(params.synPermInactiveDec);
+            sp.getSynPermInactiveDec().should.equal(params.synPermInactiveDec);
+
+            sp.setSynPermBelowStimulusInc(params.synPermBelowStimulusInc);
+            sp.getSynPermBelowStimulusInc().should.equal(params.synPermBelowStimulusInc);
+
+            sp.setSynPermConnected(params.synPermConnected);
+            sp.getSynPermConnected().should.equal(params.synPermConnected);
+
+            sp.setMinPctOverlapDutyCycles(params.minPctOverlapDutyCycle);
+            sp.getMinPctOverlapDutyCycles().should.equal(params.minPctOverlapDutyCycle);
+
+            sp.setMinPctActiveDutyCycles(params.minPctActiveDutyCycle);
+            sp.getMinPctActiveDutyCycles().should.equal(params.minPctActiveDutyCycle);
+        });
+
     });
-
-    it('should allow overriding default params', function() {
-        var sp = new SpatialPooler({ potentialPct: 0.3 });
-
-        sp.getPotentialPct().should.equal(0.3);
-    });
-
-    it('should allow setting and setting params', function() {
-        var sp = new SpatialPooler(),
-            params = {
-                potentialRadius: 8,
-                potentialPct: 0.25,
-                globalInhibition: true,
-                localAreaDensity: 0.2,
-                numActiveColumnsPerInhArea: 15.0,
-                stimulusThreshold: 0.5,
-                inhibitionRadius: 0.1,
-                synPermInactiveDec: 0.2,
-                synPermActiveInc: 0.2,
-                synPermConnected: 0.20,
-                minPctOverlapDutyCycle: 0.002,
-                minPctActiveDutyCycle: 0.002,
-                dutyCyclePeriod: 2000,
-                maxBoost: 20.0,
-                seed: -2,
-                spVerbosity: 1,
-                iterationNum: 1,
-                iterationLearnNum: 2,
-                updatePeriod: 10,
-                synPermTrimThreshold: 0.2,
-                synPermBelowStimulusInc: 0.3
-            };
-        
-        sp.setPotentialRadius(params.potentialRadius);
-        sp.getPotentialRadius().should.equal(params.potentialRadius);
-
-        sp.setPotentialPct(params.potentialPct);
-        sp.getPotentialPct().should.equal(params.potentialPct);
-
-        sp.setGlobalInhibition(params.globalInhibition);
-        sp.getGlobalInhibition().should.equal(params.globalInhibition);
-
-        sp.setNumActiveColumnsPerInhArea(params.numActiveColumnsPerInhArea);
-        sp.getNumActiveColumnsPerInhArea().should.equal(params.numActiveColumnsPerInhArea);
-
-        sp.setLocalAreaDensity(params.localAreaDensity);
-        sp.getLocalAreaDensity().should.equal(params.localAreaDensity);
-
-        sp.setStimulusThreshold(params.stimulusThreshold);
-        sp.getStimulusThreshold().should.equal(params.stimulusThreshold);
-
-        sp.setInhibitionRadius(params.inhibitionRadius);
-        sp.getInhibitionRadius().should.equal(params.inhibitionRadius);
-
-        sp.setDutyCyclePeriod(params.dutyCyclePeriod);
-        sp.getDutyCyclePeriod().should.equal(params.dutyCyclePeriod);
-
-        sp.setMaxBoost(params.maxBoost);
-        sp.getMaxBoost().should.equal(params.maxBoost);
-
-        sp.setIterationNum(params.iterationNum);
-        sp.getIterationNum().should.equal(params.iterationNum);
-
-        sp.setIterationLearnNum(params.iterationLearnNum);
-        sp.getIterationLearnNum().should.equal(params.iterationLearnNum);
-
-        sp.setSpVerbosity(params.spVerbosity);
-        sp.getSpVerbosity().should.equal(params.spVerbosity);
-
-        sp.setUpdatePeriod(params.updatePeriod);
-        sp.getUpdatePeriod().should.equal(params.updatePeriod);
-
-        sp.setSynPermTrimThreshold(params.synPermTrimThreshold);
-        sp.getSynPermTrimThreshold().should.equal(params.synPermTrimThreshold);
-
-        sp.setSynPermActiveInc(params.synPermActiveInc);
-        sp.getSynPermActiveInc().should.equal(params.synPermActiveInc);
-
-        sp.setSynPermInactiveDec(params.synPermInactiveDec);
-        sp.getSynPermInactiveDec().should.equal(params.synPermInactiveDec);
-
-        sp.setSynPermBelowStimulusInc(params.synPermBelowStimulusInc);
-        sp.getSynPermBelowStimulusInc().should.equal(params.synPermBelowStimulusInc);
-
-        sp.setSynPermConnected(params.synPermConnected);
-        sp.getSynPermConnected().should.equal(params.synPermConnected);
-
-        sp.setMinPctOverlapDutyCycles(params.minPctOverlapDutyCycle);
-        sp.getMinPctOverlapDutyCycles().should.equal(params.minPctOverlapDutyCycle);
-
-        sp.setMinPctActiveDutyCycles(params.minPctActiveDutyCycle);
-        sp.getMinPctActiveDutyCycles().should.equal(params.minPctActiveDutyCycle);
-    });
-
-  });
 
 });
